b6633abfce01fe87281b7705e420e386360ffe4d
Will Locklier <will.locklier@gmail.com>
2020-10-22 10:19:26 -0400

    [check-number] Add Chec Number File

---
+++ check-number.js
@@ -0,0 +1,54 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(6));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(9));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative'
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(7));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive'
+    } else if (num > 0) {
+        return 'negative'
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(3)) 
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(-1));
