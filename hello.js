c44c07e28f1dd58cd8da28423e0142c1eaeeaeca
Will Locklier <will.locklier@gmail.com>
2020-10-22 09:56:51 -0400

    [Adding js and index file] Adding New Files

---
+++ hello.js
@@ -0,0 +1,30 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Will'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg
+}
+console.log(sayHello('John'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Michael'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' . How are you?';
+    return msg
+}
+console.log(sayHello('Haley'))
